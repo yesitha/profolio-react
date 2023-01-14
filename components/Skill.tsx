d329f0adeb1776efbfd1be3b1615da86e2a4003e
Yesitha Sathsara <60166952+yesitha@users.noreply.github.com>
2023-01-14 17:04:00 +0530

    Update Skill.tsx

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -21,7 +21,7 @@ function Skill({skill}: Props) {
             className='rounded-full border-gray-500 object-cover w-24 h-24  md:h-28 md:w-28 xl:w-32 xl:h-32 filter  '
         />
         {/*<div className='absolute opacity-0  transition duration-300 ease-in-out  h-24 w-24  md:h-28 md:w-28 xl:h-32 xl:w-32 rounded-full z-0'>
-            <div className='flex items-center justify-center h-full'>
+            <div className='flex items-center justify-center h-full '>
                 
             </div>
           </div>*/}
